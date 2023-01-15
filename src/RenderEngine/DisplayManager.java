5c6ed57c260e1cf2b119d6423d3aa1b7637986d9
Won Jin Kim <wonjin.kim@mail.utoronto.ca>
2023-01-14 22:44:17 -0500

    initial commit

---
+++ src/RenderEngine/DisplayManager.java
@@ -0,0 +1,50 @@
+package RenderEngine;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
+import org.lwjgl.opengl.*;
+
+public class DisplayManager {
+    private static final int WIDTH = 120;
+    private static final int HEIGHT = 720;
+    private static final int FPS_CAP = 120;
+
+    public static void createDisplay() {
+        ContextAttribs attribs = new ContextAttribs(3,2)
+                .withForwardCompatible(true)
+                .withProfileCore(true);
+        try {
+            Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
+            Display.create(new PixelFormat(), attribs);
+            Display.setTitle("VoxelTutorial");
+            Display.setFullscreen(true);
+            GL11.glViewport(0, 0, Display.getWidth(), Display.getHeight());
+        } catch (LWJGLException e) {
+            throw new RuntimeException(e);
+        }
+        Mouse.setGrabbed(true);
+    }
+
+    public static void updateDisplay() {
+        Display.sync(FPS_CAP);
+        Display.update();
+
+        while (Keyboard.next()) {
+            if(Keyboard.getEventKeyState())
+            {
+                if(Keyboard.isKeyDown(Keyboard.KEY_E) && Mouse.isGrabbed())
+                {
+                    Mouse.setGrabbed(false);
+                } else {
+                    Mouse.setGrabbed(true);
+                }
+            }
+
+        }
+    }
+    public static void closeDisplay() {
+        Display.destroy();
+        System.exit(0);
+    }
+}
